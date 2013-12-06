5be9bf444f688b9f17befb6e7c952be5c9de7c48
javason <javason@hanmail.net>
2013-12-06 10:15:36 +0900

    커밋 테스트2

--- src/net/javason/hello.java
+++ src/net/javason/hello.java
@@ -5,6 +5,6 @@ package net.javason;
  */
 public class hello {
     public static void main(String args[]) {
-        System.out.println("hello");
+        System.out.println("hello world 안녕 ");
     }
 }
